dcf8d1ecd5f01f3e28cfebf13d9c2f2492e371d3
unknown <cudzich_m@outlook.com>
2020-04-24 21:13:59 -0500

    Read through the underscore.js library

--- Section 4 - Objects and Functions/52- Functional Programming (Part 2)/app.js
+++ Section 4 - Objects and Functions/52- Functional Programming (Part 2)/app.js
@@ -1,73 +1,7 @@
-function mapForEach(arr, fn) {
-    
-    var newArr = [];
-    for (var i=0; i < arr.length; i++) {
-        newArr.push(
-            fn(arr[i])   
-        )
-    };
-    
-    return newArr;
-}
+_.times(3, function() { console.log('hi')});
 
-var arr1 = [1,2,3];
-console.log(arr1);
+var arr = _.map([1,2,3], function(item) { return item*2 });
+console.log(arr);
 
-
-var arr2 = mapForEach(arr1, function(item) {
-   return item * 2; 
-});
-console.log(arr2);
-
-
-var arr3 = mapForEach(arr1, function(item) {
-   return item > 2; 
-});
-console.log(arr3);
-
-
-var checkPastLimit = function(limiter, item) {
-    return item > limiter;   
-}
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
-console.log(arr4);
-
-
-var checkPastLimitSimplified = function(limiter) {
-    return function(limiter, item) {
-        return item > limiter;   
-    }.bind(this, limiter); 
-};
-
-var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
-console.log(arr5);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var random = _.random(1,10);
+console.log('random: ', random);
